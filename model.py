bad3b67f571673e3fae700b34628e12d406e4bd7
kinshukphalke <phalkekinshuk@gmail.com>
2022-06-23 20:49:15 -0400

    Transitioning to Python rip

---
+++ model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+class ActorCriticModel(keras.Model):
+  def __init__(self, state_size, action_size):
+    super(ActorCriticModel, self).__init__()
+    self.state_size = state_size
+    self.action_size = action_size
+    self.dense1 = layers.Dense(100, activation='relu')
+    self.policy_logits = layers.Dense(action_size)
+    self.dense2 = layers.Dense(100, activation='relu')
+    self.values = layers.Dense(1)
+
+  def call(self, inputs):
+    x = self.dense1(inputs)
+    logits = self.policy_logits(x)
+    v1 = self.dense2(inputs)
+    values = self.values(v1)
+    return logits, values
